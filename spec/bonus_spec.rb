98d3164832d4d7c8dfe7f2904f5a82c65f8e2ee3
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-28 22:35:30 +0000

    Automatically backed up by Learn

--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -2,8 +2,13 @@ require_relative 'spec_helper'
 require_relative '../lib/bonus.rb'
 
 describe "bonus" do
+<<<<<<< HEAD
   # remove the 'x' from the 'xit' below to activate this test!
 	it "uses the []= method to change the hero and heroine's status from alive to dead" do
+=======
+   remove the 'x' from the 'xit' below to activate this test!
+	xit "uses the []= method to change the hero and heroine's status from alive to dead" do
+>>>>>>> 7f9c7b6b61a1c4966a5d408e60c1ed31e9b3111e
     expect(bonus[:montague][:hero][:status]).to eq("dead")
     expect(bonus[:capulet][:heroine][:status]).to eq("dead")
 
